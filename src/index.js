b2219ab7ecc65d976e59701b9ad584e3ab0098c6
TaraMKelly <tara.kelly16@gmail.com>
2022-03-07 11:47:10 -0500

    basic components and styles added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom"
+import './index.css'
+
+
+const Square = () => {
+  return (
+    <div style ={{
+      backgroundColor: 'gold',
+      margin: 10,
+      padding: 20
+    }}>
+      Square
+    </div>
+  )
+}
+
+const Board = () => {
+  return (
+    <div style = {{
+      backgroundColor: 'skyblue',
+      margin: 10,
+      padding: 20
+    }}>
+      <Square />
+    </div>
+  )
+}
+
+const Game = () => {
+  return (
+    <div style={{
+      backgroundColor: "salmon",
+      margin: 10,
+      padding: 20
+    }}>
+      <Board /> 
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
