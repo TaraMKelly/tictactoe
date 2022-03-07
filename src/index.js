b56bd410dee556e126393c6ec0fcc43c77c24798
TaraMKelly <tara.kelly16@gmail.com>
2022-03-07 12:13:08 -0500

    added 9 squares, more styling

--- src/index.js
+++ src/index.js
@@ -2,38 +2,54 @@ import ReactDOM from "react-dom"
 import './index.css'
 
 
-const Square = () => {
+const Square = ({value}) => {
   return (
-    <div style ={{
-      backgroundColor: 'gold',
-      margin: 10,
-      padding: 20
-    }}>
-      Square
-    </div>
+    <button 
+      className="square"
+      onClick={alert(`square ${value} was clicked`)}
+      >
+      {value}
+    </button>
   )
 }
 
 const Board = () => {
+  const renderSquare = (i) => {
+    return (
+      <Square value={i}/>
+    )
+  }
+
   return (
-    <div style = {{
+    <div style={{
       backgroundColor: 'skyblue',
       margin: 10,
       padding: 20
     }}>
-      <Square />
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
     </div>
   )
 }
 
 const Game = () => {
   return (
-    <div style={{
-      backgroundColor: "salmon",
-      margin: 10,
-      padding: 20
-    }}>
-      <Board /> 
+    <div className="game">
+      Game
+      <Board />
     </div>
   )
 }
